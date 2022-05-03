505ca3e15db6e018c69fcba581298927864dc143
PJA <p.j.aanhane@student.tudelft.nl>
2022-05-03 09:41:49 +0200

    updated function to convert bci data to numpy array

--- Authentication/Code/data_to_numpy.py
+++ Authentication/Code/data_to_numpy.py
@@ -1,9 +1,13 @@
 from pathlib import Path
-import csv
 import numpy as np
 
-TXT_PATH = ".\\recorded_data\\recordings_txt\OpenBCISession_Sam_take_2\OpenBCI-RAW-2022-05-02_15-07-38.txt"
-test = np.array([])
-with open(TXT_PATH, 'r') as f:
-        np_frame = np.loadtxt(fname=f, skiprows=5, delimiter=', ', usecols=(i for i in range(1,9)))
-        np.save('.\\recorded_data\\recordings_numpy\\test.npy', np_frame)
+RECORDINGS_PATH = Path('./recorded_data/recordings_txt')
+
+RECORDING_FOLDERS = list(RECORDINGS_PATH.iterdir())
+
+for recording in RECORDING_FOLDERS:
+        path = list(recording.glob('*.txt'))[0]
+        with open(path, 'r') as file:
+                np_frame = np.loadtxt(fname=file, skiprows=5, delimiter=', ', usecols=(i for i in range(1,9)))
+                save_path = Path(f'./recorded_data/recordings_numpy/{path.stem}.npy')
+                np.save(save_path, np_frame)

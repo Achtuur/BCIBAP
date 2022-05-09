d91351c382e50bda18d32e61d32ffa44834ef7e3
Chris <47724503+Debotten@users.noreply.github.com>
2022-05-09 10:18:54 +0200

    Change location of label extraction

---
+++ signal_processing/EEGLAB/Label-function-CHB-Data/Label_extraction_CHB.m
@@ -0,0 +1,9 @@
+% Extract the seizure times and output them based on Epoch length
+
+clearvars;
+
+
+% [RegexOut,SmplRate,LabelOut] = LabelOld('chb01-summary.txt');
+
+EpochLength = 1; % Length of epoch in seconds
+[SmplRate,LabelOut] = Label_extract('chb04-summary.txt',EpochLength);
--- signal_processing/Label-function-CHB-Data/Label_extraction_CHB.m
+++
@@ -1,9 +0,0 @@
-% Extract the seizure times and output them based on Epoch length
-
-clearvars;
-
-
-% [RegexOut,SmplRate,LabelOut] = LabelOld('chb01-summary.txt');
-
-EpochLength = 1; % Length of epoch in seconds
-[SmplRate,LabelOut] = Label_extract('chb04-summary.txt',EpochLength);

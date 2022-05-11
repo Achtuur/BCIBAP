8794aaf26b48ec4b3f2035fa06e1e57ba4eab50f
smolenkamp <74531943+smolenkamp@users.noreply.github.com>
2022-05-11 22:07:11 +0200

    wavelet transform

--- Authentication/Code/FeatureExtraction/wavelet.py
+++ Authentication/Code/FeatureExtraction/wavelet.py
@@ -1,4 +1,7 @@
+from turtle import shape
+from pprint import pprint
 import numpy as np
+import scipy as sp
 import matplotlib
 import matplotlib.pyplot as plt
 import pywt
@@ -9,37 +12,41 @@ from prepare_data import crop
 from Filters import Filter
 from Pipeline import Pipeline
 from Visualize import DataPlot
-
+ 
 class Wavelet():
-    @staticmethod
-    def doppler(freqs, dt, amp_inc=10, t0=0, f0=np.pi*2):
-        t = np.arange(len(freqs)) * dt + t0
-        amp = np.linspace(1, np.sqrt(amp_inc), len(freqs))**2
-        sig = amp * np.sin(freqs * f0 * t)
-        return t,sig
 
-    @staticmethod 
-    def noisify(sig, noise_amp=1):
-        return sig + (np.random.random(len(sig))-0.5)*2*noise_amp
-    
-
-    
     @staticmethod
-    def coeff(data, plot=False):
+    def coefficients(data, plot=False):
         # coeff = pywt.wavedec(data, 'db2', level=4)
-        cA, cD = pywt.dwt(data, "sym12", mode="zero")
+        coeff = pywt.wavedec(data, "db2", level=4)
         if plot == True:
-            Wavelet.plot_wavelet(data, cA, cD)
+            Wavelet.plot_wavelet(data, coeff[0], coeff[1])
         return coeff
     
     @staticmethod
     def plot_wavelet(data, cA, cD):
-        plt.plot(data, label="raw_data")
         plt.plot(cA, label="approximation coefficients")
         plt.plot(cD, label="detail coefficients")
         plt.legend(loc="best")
         plt.show()
 
+    @staticmethod
+    def stats(coeff):
+        statlib = {}
+        for i in range(len(coeff)):
+            MAV = np.mean(np.abs(coeff[i]))
+            AVP = np.mean(np.square(coeff[i]))
+            SD = np.std(coeff[i])
+            VAR = np.var(coeff[i])
+            SKEW = sp.stats.skew(coeff[i])
+            KURT = sp.stats.kurtosis(coeff[i])
+            statlib[f'D{i}'] = [MAV, AVP, SD, VAR, SKEW, KURT]
+        return statlib
+
+
+
+
+        # print(np.shape(MAV))
 
 if __name__ == "__main__":
 # Initialise config variables
@@ -61,7 +68,10 @@ if __name__ == "__main__":
     raw = np.concatenate((cropped_data[2], cropped_data[3], cropped_data[4], cropped_data[5]))
     # raw = np.concatenate((cropped_data[2], cropped_data[3]))
     eeg_data = Pipeline(raw, cal_eeg_data).start(plot=False)
+    print(eeg_data.shape)
     one_channel = eeg_data[:, 0]
-    coefficients = Wavelet.coeff(one_channel, plot=True)
-    # t_dop, sig_dop = doppler(np.arange(10,20,0.01)[::-1], 0.002)
-    # sig_dop_n2 = noisify(sig_dop, noise_amp=2)
+    coefficients = Wavelet.coefficients(one_channel, plot=True)
+    stats = Wavelet.stats(coefficients)
+    pprint(stats)
+
+

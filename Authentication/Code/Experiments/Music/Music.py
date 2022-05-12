465769e2db67a66f4b355a5ead607ed8556a88de
PJA <p.j.aanhane@student.tudelft.nl>
2022-05-12 20:01:46 +0200

    Adaptations to music experiment

--- Authentication/Code/Experiments/Music/Music.py
+++ Authentication/Code/Experiments/Music/Music.py
@@ -7,80 +7,101 @@ import time
 from datetime import datetime
 import numpy as np
 import csv
+from pathlib import Path
+
+# This is to easily run the script from the command line
+# [] = optional
+# Usage: python Music.py -name file_name -known known_song [-time] time [-song_dur] song_duration
+import argparse
+
+# Create parser
+my_parser = argparse.ArgumentParser(description="Code to run music experiment")
+my_parser.add_argument('-song_dur',
+                        required=False,
+                        default=5,
+                        metavar='-duration song sample',
+                        type=int,
+                        help='The duration each sample is played'    
+                    )
+my_parser.add_argument('-pause',
+                        required=False,
+                        default=5,
+                        metavar='-pause',
+                        type=int,
+                        help='The pause between each sample'    
+                    )
+my_parser.add_argument('-name',
+                        required=True,
+                        metavar='-file name',
+                        type=str,
+                        help='The file name'
+)
+my_parser.add_argument('-known',
+                        required=True,
+                        metavar='-known song',
+                        type=int,
+                        help='The number of the known song'
+)
+args = my_parser.parse_args()
+
+def get_song_amount():
+    song_dir = Path('./songs')
+    files = os.listdir(song_dir)
+
+    # Get list of all files which contain .mp3 and subtract 
+    num_songs = len([x for x in files if ".mp3" in x])
+    return num_songs
+
 
 #change parameters of experiment
-songs_amount = 8 #amount of songs in the songs folder
-songtime = 5 #duration of song in seconds, max number is duration of song minus a minute
-pause = 5 #time between song fragments, at least 3
-known_song = 8 #number of the song that is known by the user
+songs_amount = get_song_amount() #amount of songs in the songs folder
+songtime = args.song_dur #duration of song in seconds, max number is duration of song minus a minute
+pause = args.pause #time between song fragments, at least 3
+known_song = args.known #number of the song that is known by the user
 
-amount = list(range(1,songs_amount + 1))
-songs = [0] * songs_amount
-j = 0
+songs = [0 for i in range(songs_amount)]
 
-for i in amount:
+for i in range(1, songs_amount + 1):
     start = random.randint(0,60)
     starttime = 60*1000 + start*1000
     endtime = 60*1000 + (start + songtime)*1000
-    #print(starttime)
-    #print(endtime)
-    filename = ".\songs\Song"
-    #print(filename)s
-    filename = [filename, str(i), '.mp3']
-    filename = ''.join(filename)
-    #print(filename)
-    filename = os.path.abspath(os.path.join(os.getcwd(), filename))
-    #print(filename)
+    filename = Path(f"./songs/Song{str(i)}.mp3")
     sound = AudioSegment.from_mp3(filename)
-    songs[j] = sound[starttime:endtime]
-    newname = ['song', str(i), '.mp3']
-    newname = ''.join(newname)
-    songs[j].export(newname, format="mp3")
-    j = j + 1
+    songs[i-1] = sound[starttime:endtime]
+    newname = Path(f'./song_samples/song{str(i)}.mp3')
+    songs[i-1].export(newname, format="mp3")
 
-data = [0] * songs_amount
+data = [0 for i in range(songs_amount)]
 
-filename = datetime.now()
-filename = filename.strftime("%d/%m/%Y %H:%M:%S")
-filename = filename.replace("/", "-")
-filename = filename.replace(" ", "_")
-filename = filename.replace(":", "-")
+file = Path('./experiment_song_order')
 
-times = [0] * songs_amount
-bitjes = [0] * songs_amount
+times = [0 for i in range(songs_amount)]
+bitjes = [0 for i in range(songs_amount)]
 j = 0
-random.shuffle(amount)
-for i in amount:
-    songname = ['song', str(i), '.mp3']
-    songname = ''.join(songname)
-    if songname == f'song{known_song}.mp3':
-        bitjes[j] = 1
+exp_song_order = [i for i in range(1, songs_amount+1)]
+random.shuffle(exp_song_order)
+for index, num in enumerate(exp_song_order):
+    songname = Path(f'song_samples/song{str(num)}.mp3')
+    if songname == Path(f'song_samples/song{known_song}.mp3'):
+        bitjes[index] = 1
     else:
-        bitjes[j] = 0
-    data[j] = songname
+        bitjes[index] = 0
+    data[index] = songname.stem
     print('3')
     time.sleep(1)
     print('2')
     time.sleep(1)
     print('1')
-    times[j] = datetime.now()
-    times[j] = times[j].strftime("%H:%M:%S.%f")[:12]
+    times[index] = datetime.now()
+    times[index] = times[index].strftime("%H:%M:%S.%f")[:12]
     playsound(songname)
     time.sleep(pause - 3)
-    j = j + 1 
 
 all_data = [data, times, bitjes]
 all_data = np.array(all_data).T.tolist()
 
-filename = str(datetime.now())
-filename = filename.replace("/", "-")
-filename = filename.replace(" ", "_")
-filename = filename.replace(":", "-")
-filename = f'.\song_lists\songlist_{filename}'
-data_tekst = ''.join(data)
-
-with open(f'{filename}.csv', 'w') as f:
-      
+file = Path(f'./experiment_song_order/{args.name}.csv')
+with open(file, 'w') as f:
     # using csv.writer method from CSV package
     write = csv.writer(f)
     write.writerows(all_data)

2270c2c12536a9d6be8dd7257d294b2feb88d5ed
Joos_Vrijdag <joos.vrijdag@gmail.com>
2022-05-16 17:45:03 +0200

    prepare_data is working (i hope)

--- Authentication/Code/PipelineComponents/Preprocessing/prepare_data.py
+++ Authentication/Code/PipelineComponents/Preprocessing/prepare_data.py
@@ -2,6 +2,7 @@ from math import ceil
 import csv
 from pathlib import Path
 import numpy as np
+import os
 
 def crop(sample_start, sample_end, data: np.ndarray, t_window: int, f_sampling: float) -> list:
     print(len(data))
@@ -16,9 +17,7 @@ def crop(sample_start, sample_end, data: np.ndarray, t_window: int, f_sampling: 
     return cropped_data
 
 
-#This function gets labels from the pseudoword timestamp file, which are a 1 for pseudoword and a 0 for a real word
-
-# Deze functie moet een path als input hebben zodat het niet gehardcode hoeft te zijn
+#This function gets labels from the timestamp file
 def get_labels(path_experiment, label_column = 1):
     path = Path(path_experiment)
     
@@ -26,29 +25,16 @@ def get_labels(path_experiment, label_column = 1):
         csv_reader = csv.reader(f, delimiter=',')
         data = list(csv_reader)
 
-    # Het is onduidelijk wat j en i doen, ik denk dat je van een lijst
-    # een index en een item wilt. Je moet dan for index, item in enumerate(data) doen,
-    # dan hoef je niet op deze manier met letter variabelen te werken
     labels = [0] * int((0.5 * len(data)))
-
-    # Het is onduidelijk wat j en i doen, ik denk dat je van een lijst
-    # een index en een item wilt. Je moet dan for index, item in enumerate(data) doen,
-    # dan hoef je niet op deze manier met letter variabelen te werken
     
-    for j,i in enumerate(data): #This for loop gets the zeros and ones from the imported file and stores them as labels
-        if not i: 
-            i = ''
+    for index,label in enumerate(data): #This for loop gets the zeros and ones from the imported file and stores them as labels
+        if not label: 
+            continue
         else:
-            # Het is nice als deze 1 ook een functie input is, mocht de format verwijderen gaat
-            # het niet stuk dan
-            labels[j] = i[label_column]
-            # Dit zou dan weg kunnen
-        
+            labels[index] = int(label[label_column])
     return labels
 
-#This function gets timestamps from the file pseudoword timestamp file
-# Deze functie moet een path als input hebben zodat het niet gehardcode hoeft te zijn
-# time_column is 2
+#This function gets timestamps from the file timestamp file
 def timestamps_experiment(path_experiment, time_column = 2):
     
     with open(path_experiment, newline = '') as f:
@@ -57,36 +43,16 @@ def timestamps_experiment(path_experiment, time_column = 2):
 
     start_experiment = data[0][time_column].replace(':','').replace('.','')
     len_experiment = int(len(data)*0.5)
-    # #This for loop gets the timestamps from the experiment data and turns it into a string of only numbers
-    # # Het is onduidelijk wat j en i doen, ik denk dat je van een lijst
-    # # een index en een item wilt. Je moet dan for index, item in enumerate(data) doen,
-    # # dan hoef je niet op deze manier met letter variabelen te werken
-    # experiment_times = [0] * int((0.5 * len(data))) 
-
-    # # Het is onduidelijk wat j en i doen, ik denk dat je van een lijst
-    # # een index en een item wilt. Je moet dan for index, item in enumerate(data) doen,
-    # # dan hoef je niet op deze manier met letter variabelen te werken
     
-    # for j,i in enumerate(data):
-    #     if not i: 
-    #         i = ''
-    #     else:
-    #         # Het is nice als deze 2 ook een functie input is, mocht de format verwijderen gaat
-    #         # het niet stuk dan
-    #         experiment_times[j] = i[time_column].replace(':','').replace('.','')
-
     return start_experiment, len_experiment
 
 #This function checks at what sample of the recorded data the experiment has started
-# Is fs sampling rate? yep
-# RAW tells if the data used is raw
 def start_data(fs, path_experiment, path_recording, duration = 5): 
     # Hier mag dus een mooie input
     start_experiment, len_experiment = timestamps_experiment(path_experiment)
-    print(len_experiment)
 
     #start_data = timestamps_data(path_recording, lines_to_skip, time_location, time_information)
-    start_data = '113023148'
+    start_data = timestamps_recording(path_recording)
     time_diff = str(int(start_experiment) - int(start_data))
     time_diff = '00000' + time_diff
     ms = time_diff[-3:]
@@ -94,39 +60,23 @@ def start_data(fs, path_experiment, path_recording, duration = 5):
     m = time_diff[-7:-5]
     if s == '' and m == '':
         sample_start = int( int(ms)*(fs/1000)) #sample_start is an integer for at what sample the experiment started
-        print('dit gaat niet goed')
     elif m == '':
         sample_start = int(int(s)*fs + int(ms)*(fs*0.001))
-        print('test')
     else:
         sample_start = int(int(m)*fs*60 + int(s)*fs + int(ms)*(fs/1000))
-        print('heel raar dit')
 
     sample_end = len_experiment*fs*duration + sample_start
-    print(sample_start)
-    print(sample_end)
     return sample_start, sample_end
 
 
 #This function takes data from the raw recording file and takes the timestamps from this, it removes the dates and turns the time into a string of only numbers
-# lines to skip is in principe 5, voor de rauwe data
-# time_location is in principe 24, voor rauwe data
-# time information geeft aantal getallen aan met tijd informatie 3 = miliseconde, 5 = seconde , 7 = minuten, 9 = uren. Uren is standaard
-def timestamps_data(path_recording, lines_to_skip = 5, time_location = 24, time_information = 9):
+def timestamps_recording(path_recording, lines_to_skip = 5, time_location = 24, time_information = 9):
     path = Path(path_recording)
     with open(path, newline = '') as f: 
         data = f.readlines()
 
     time_information = len(data[5]) - time_information
     start_data = data[lines_to_skip[time_location]][:time_information]   
-        # Hier ook weer een list comprehension
-        # data_times = [0] * (len(data) - 5)
-
-    # for j, i in enumerate(data[lines_to_skip:]):
-    #     i = i.split(",")
-    #     data_times[j] = i[time_location].replace('\n','').replace(' ','').replace('-','').replace(':','').replace('.','')
-    #     time_information = len(data_times[j]) - time_information
-    #     data_times[j] = data_times[j][:time_information]
     return start_data
 
 

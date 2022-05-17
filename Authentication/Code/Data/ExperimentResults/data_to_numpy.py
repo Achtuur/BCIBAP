49e6bf73373b2fec3d684f8bbe53bdeff7fbc3c1
PJA <p.j.aanhane@student.tudelft.nl>
2022-05-17 16:08:15 +0200

    added comments to the data_to_numpy file

--- Authentication/Code/Data/ExperimentResults/data_to_numpy.py
+++ Authentication/Code/Data/ExperimentResults/data_to_numpy.py
@@ -25,14 +25,22 @@ RECORDINGS_CROP_DICTIONARY = {
 }
 
 def txt_to_numpy(recording_txt_path, filename, subject_name):
+        # This is the path to numpy recordings folder
         BASE_PATH = Path('./recorded_data/recordings_numpy')
+
+        # This is the path to the folder that will be created for the different npy files
         subject_dir = BASE_PATH / Path(subject_name)
+
+        # If the folder doesn't exist yet, it is created
         try:
                 os.makedirs(subject_dir)
         except FileExistsError:
                 pass
         
+        # The raw recording folders only have one file, so this gets the txt file 
         raw_txt_file = list(recording_txt_path.iterdir())[0]
+        
+        # These lines open the txt file, skip the first 5 rows, and save the 1-8 columns as a npy file
         with open(raw_txt_file, 'r') as file:
                 np_frame = np.loadtxt(fname=file, skiprows=5, delimiter=', ', usecols=(i for i in range(1,9)))
                 save_path = Path(f'{subject_dir}/{filename}.npy')
@@ -40,27 +48,33 @@ def txt_to_numpy(recording_txt_path, filename, subject_name):
 
 
 if __name__ == '__main__':
-        # Raw recording path
+        # Raw recording path to folder containing subjects
         RAW_RECORDING_PATH = Path('./recorded_data/recordings_txt')
 
         # Iterate Test Subjects
         SUBJECTS = RAW_RECORDING_PATH.iterdir()
         for subject in SUBJECTS:
+                # Name of folder in recordings_txt
                 subject_name = subject.stem
+
+                # Path to the upper folder
                 base_path = RAW_RECORDING_PATH / Path(subject_name)
-                # Iterate recordings
+                
+                # Iterate recording folders in the subject folder
                 RECORDINGS = base_path.iterdir()
                 for recording in RECORDINGS:
+                        # This is the path to the recording txt file
                         recording_path = Path(recording)
+
+                        # This is the name of the folder the npy file will be created in
                         folder_name = recording_path.stem
                         txt_to_numpy(recording_path, folder_name, subject_name)
 
+        # This code iterates the global dictionary and uses it to crop all the recordings which must be cropped
         for recording_to_crop in RECORDINGS_CROP_DICTIONARY.values():
                 numpy_data = np.load(recording_to_crop['path'])
-                print(f"{recording_to_crop['path']} --- {numpy_data.shape}")
                 cropped_numpy_data = numpy_data[recording_to_crop['crop'][0]:recording_to_crop['crop'][1]]
                 np.save(recording_to_crop['path'], cropped_numpy_data)
-                print(np.load(recording_to_crop['path']).shape)
                 
         
 

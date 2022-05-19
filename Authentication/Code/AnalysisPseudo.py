8fa3513385093056d38dd10f167e31198fa95abb
smolenkamp <74531943+smolenkamp@users.noreply.github.com>
2022-05-19 15:19:37 +0200

    commit to show checkout

--- Authentication/Code/AnalysisPseudo.py
+++ Authentication/Code/AnalysisPseudo.py
@@ -13,7 +13,9 @@ import platform
 if platform.system() == "Windows":
     sys.path.append(str(Path('PipelineComponents/Preprocessing').resolve()))
     sys.path.append(str(Path('PipelineComponents/FeatureExtraction').resolve()))
+    sys.path.append(str(Path('PipelineComponents/Classification').resolve()))
     sys.path.append(str(Path('Data/ExperimentResults').resolve()))
+    sys.path.append(str(Path('Data').resolve()))
 else:
     sys.path.append(str(Path('./PipelineComponents/Preprocessing').resolve()))
     sys.path.append(str(Path('./PipelineComponents/FeatureExtraction').resolve()))
@@ -34,7 +36,7 @@ from FeaturePipeline import FeaturePipeline
 
 # Classification
 from SVM import svm_classifier
-
+from Kfold_CV import Models
 # Auxillary functions
 def get_labels(path_experiment, label_column = 1):
     path = path_experiment
@@ -75,11 +77,11 @@ if __name__ == '__main__':
     # M1
     EXPERIMENT_M1 = ExperimentWrapper("M1", 
         'Pseudo',    
-        Path('./Data/ExperimentResults/recorded_data/recordings_numpy/M1_10_05_2022/OpenBCISession_Wessel_calibration.npy'),
+        Path('./Data/ExperimentResults/recorded_data/recordings_numpy/M1_10_05_2022/OpenBCISession_M1_calibration.npy'),
     )
 
     EXPERIMENT_M1 = EXPERIMENT_M1.set_experiment_data(
-        Path('./Data/ExperimentResults/recorded_data/recordings_numpy/M1_10_05_2022/OpenBCISession_Wessel_pseudo.npy')
+        Path('./Data/ExperimentResults/recorded_data/recordings_numpy/M1_10_05_2022/OpenBCISession_M1_pseudo.npy')
     ).set_experiment_description_file(
         Path('./Data/Experiments/Pseudowords/results/data_2022-05-10_11-31-17.544482.csv')
     )
@@ -127,56 +129,65 @@ if __name__ == '__main__':
         
         # This is not correct
         cropped_data = crop(data, 5, 250)
-                
+        print(experiment.get_subject(), len(cropped_data), len(cropped_data[0]), len(cropped_data[0][0]))      
         # Calculate Features, reshaped to 240x1 numpy array
         for data_interval in cropped_data:
+            data_interval = data_interval.reshape(1, 10000)
             try:
-                features = np.vstack([features, np.array(FeaturePipeline(data_interval).start()).reshape(1,240)])
+                # features = np.vstack([features, np.array(FeaturePipeline(data_interval).start()).reshape(1,240)])
+                features = np.vstack([features, np.array(data_interval)])
             except ValueError:
-                features = np.array(FeaturePipeline(data_interval).start()).reshape(1,240)
+                # features = np.array(FeaturePipeline(data_interval).start()).reshape(1,240)
+                features = np.array(data_interval)
 
         
         labels = labels + get_labels(experiment.get_experiment_description_file())
-
+        print(features.shape, len(labels))
+    print(features.shape, len(labels))
+    # features_normalized = normalize(features, axis=0)
     
-    features_normalized = normalize(features, axis=0)
-
     # This is arbitrarily chosen and depends on how many features you have
-    pca = PCA(n_components=80)
-    principal_components = pca.fit_transform(features_normalized)
-    principal_components = principal_components.tolist() 
+    # pca = PCA(n_components=80)
+    # principal_components = pca.fit_transform(features_normalized)
+    # principal_components = principal_components.tolist() 
 
     # Randomise data
-    training_data = list(zip(principal_components, labels))
+    training_data = list(zip(features, labels))
     random.shuffle(training_data)
-    principal_components[:], labels[:] = zip(*training_data)
+    features[:], labels[:] = zip(*training_data)
 
     # Create train and test data
-    total_length = len(training_data)
-    split_percentage = 0.6
+    X_train, X_test, Y_train, Y_test = Models.train_val_split(features, labels, test_size = 0.2)
+    # X_train, X_val, Y_train, Y_val = Models.train_val_split(X_to_split, Y_to_split, test_size = 0.25)
 
-    split = floor(split_percentage * total_length)
-    train_data = principal_components[:split]
-    train_labels = labels[:split]
+    # print(X_train.shape)
+    # total_length = len(training_data)
+    # split_percentage = 0.6
 
-    test_data = principal_components[split+1:]
-    test_labels = labels[split+1:]
+    # split = floor(split_percentage * total_length)
+    # train_data = principal_components[:split]
+    # train_labels = labels[:split]
+
+    # test_data = principal_components[split+1:]
+    # test_labels = labels[split+1:]
     #-------------------------------------------------------------------- 
 
 
     # Below can be changed freely
     #-------------------------------------------------------------------- 
     # # Train classifier
-    svm = svm_classifier(train_data, train_labels)
+    model = Models()
+    model.KFOLD_CV(X_train, Y_train, X_test, Y_test)
+    # svm = svm_classifier(train_data, train_labels)
 
-    result = []
-    for test in test_data:
-        result.append(svm.predict(np.array([test])))
+    # result = []
+    # for test in test_data:
+    #     result.append(svm.predict(np.array([test])))
 
-    correct = 0
-    for i,j in list(zip(test_labels, result)):
-        if i == j[0]:
-            correct += 1
+    # correct = 0
+    # for i,j in list(zip(test_labels, result)):
+    #     if i == j[0]:
+    #         correct += 1
 
-    print(f'Number of test samples: {len(test_data)}\nTotal Correct: {correct}\nAccuracy: {correct/len(test_data)}')
+    # print(f'Number of test samples: {len(test_data)}\nTotal Correct: {correct}\nAccuracy: {correct/len(test_data)}')
     #-------------------------------------------------------------------- 

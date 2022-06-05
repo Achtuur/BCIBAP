9a2c2c1d72f524e8f6caea27c27f41746f595f1e
adedespirlet <61005249+adedespirlet@users.noreply.github.com>
2022-06-05 15:14:43 +0200

    add changes regarding pictures

--- signal_processing/EEGLAB/FeatExtract/FeatExtract.m
+++ signal_processing/EEGLAB/FeatExtract/FeatExtract.m
@@ -19,6 +19,7 @@ for i=1:rowsEpoch
     x=x+1;
 end
 epoch1= epochs(2,:);
+<<<<<<< Updated upstream
 epoch2= decimate(epoch1,2);
 
 epochsDec=zeros(2406,384);
@@ -26,11 +27,14 @@ for i=1:2406
     epochsDec(i,:)=decimate(epochs(i,:),2);
 end
 
+=======
+epoch1= decimate(epoch1,2);
+>>>>>>> Stashed changes
 
 %% TIME DOMAIN SIGNAL
 t = (0:L-1)*T;        % Time vector
 figure(1)
-plot(1000*t,epochs(2,:));
+plot(1000*t,epochs(1,:));
 title('EEG signal Epoch')
 xlabel('t (milliseconds)')
 %ylabel('Amplitude')
@@ -58,96 +62,114 @@ s= spectrogram(epochs(2,:),hanning(L/3),L/6,2^nextpow2(L/3),Fs,'MinThreshold',-2
 [S,F,T] = stft(epochs(2,:),Fs,'Window',hanning(L/3),'OverlapLength',L/6,'FFTLength',2^nextpow2(L/3));
 %% WAVELET TRANSFORM
 %DWT
-% figure(3)
-% [c,l]=wavedec(epoch1,5,'db4'); %second argument is level of decomposition and 3rd is vanishing level
-% a5= appcoef(c,l,'db4');
-% [d1,d2,d3,d4,d5]=detcoef(c,l,[1 2 3 4 5]);
-% subplot(6, 1, 1)
-% plot(a5);
-% title('Approximation at Level 5') %0-4Hz
-% subplot(6, 1, 2)
-% plot(d1)
-% title('Detail Coefficients at Level 1');%64-128Hz
-% subplot(6, 1, 3)
-% plot(d2)
-% title('Detail Coefficients at Level 2');%32-64Hz
-% subplot(6, 1, 4)
-% plot(d3)
-% title('Detail Coefficients at Level 3');%16-32Hz
-% subplot(6, 1, 5)
+% Time vector
+[c,l]=wavedec(epoch1,4,'db4'); %second argument is level of decomposition and 3rd is vanishing level
+a4= appcoef(c,l,'db4');
+[d1,d2,d3,d4]=detcoef(c,l,[1 2 3 4 ]);
+% subplot(5, 1, 1)
+% plot(epoch1);
+% title('Signal epoch') %0-4Hz
+% subplot(5, 1, 2)
+% plot(a4);
+% title('Approximation coefficients at Level 4') %0-4Hz
+% subplot(5, 1, 3)
 % plot(d4)
-% title('Detail Coefficients at Level 4');%8-16Hz
-% subplot(6, 1, 6)
-% plot(d5)
-% title('Detail Coefficients at Level 5'); %4-8Hz
-% 
-% meanDelta= mean(a5); meanTheta = mean(d5); meanAlpha= mean(d4); meanBeta= mean(d3);
-% 
-% stdDelta= std(a5); stdTheta = std(d5); stdAlpha= std(d4); stdBeta= std(d3);
-% 
-% energyDelta= sum(a5.^2); energyTheta = sum(d5.^2); energyAlpha= sum(d4.^2); energyBeta= sum(d3.^2);
+% title('Detail Coefficients at Level 4');%4-8Hz
+% subplot(5, 1, 4)
+% plot(d3)
+% title('Detail Coefficients at Level 3');%8-16Hz
+% subplot(5, 1, 5)
+% plot(d2)
+% title('Detail Coefficients at Level 2');%16-32Hz
+%subplot(6, 1, 6)
+%plot(d1)
+%title('Detail Coefficients at Level 1');%32-64HHz
+%xlabel('Samples');
+%ylabel('microVolts');
+figure(3)
+t = tiledlayout('flow');
+% Plot in tiles
+nexttile, ax(1)= plot(epoch1), title('Epoch Signal','interpreter', 'latex')
+nexttile, ax(2)=  plot(a4),title('Aprroximation Coefficients at Level 4 (A4)','interpreter', 'latex')
+nexttile, ax(3)= plot(d4),title('Detail Coefficients at Level 4 (D4)','interpreter', 'latex')
+nexttile, ax(4)= plot(d3),title('Detail Coefficients at Level 3 (D3)','interpreter', 'latex')
+nexttile, ax(5)= plot(d2),title('Detail Coefficients at Level 2 (D2)','interpreter', 'latex')
+nexttile, ax(6)= plot(d1),title('Detail Coefficients at Level 1 (D1)','interpreter', 'latex')
+plotcolor(ax,'green', 'colordiff',0);
+% Specify common title, X and Y labels
+title(t, 'Decomposition of epoch signal','interpreter', 'latex','fontsize', 18)
+xlabel(t, '$Samples [n] $','interpreter', 'latex')
+ylabel(t, '$Voltage [\mu V]$','interpreter', 'latex')
+saveas(gcf,'Wavelet_decomposition','epsc')
+
+%title(tiles, "A few", 'interpreter', 'latex', 'fontsize', 18); %add title
+%meanDelta= mean(a5); meanTheta = mean(d5); meanAlpha= mean(d4); meanBeta= mean(d3);
+
+%stdDelta= std(a5); stdTheta = std(d5); stdAlpha= std(d4); stdBeta= std(d3);
+
+%energyDelta= sum(a5.^2); energyTheta = sum(d5.^2); energyAlpha= sum(d4.^2); energyBeta= sum(d3.^2);
 % 
 % entropyDelta= approximateEntropy(a5);
 % entropyTheta = approximateEntropy(d5);
 % entropyAlpha= approximateEntropy(d4);
 % entropyBeta= approximateEntropy(d3);
-% 
+
 % skewnessDelta=skewness(a5);skewnessTheta=skewness(d5);skewnessAlpha=skewness(d4);skewnessBeta=skewness(d3);
 % 
 % kurtosisDelta=kurtosis(a5);kurtosisTheta=kurtosis(d5);kurtosisAlpha=kurtosis(d4);kurtosisBeta=kurtosis(d3); 
 % DWT packet decomposition
 
-wpt = wpdec(epoch1,5,'db4','shannon');
-%plot(wpt)
-signaldelta=read(wpt,'cfs',31);
-signaltheta=read(wpt,'cfs',32);
-signalalpha=read(wpt,'cfs',33);
-signalbeta=read(wpt,'cfs',34)+read(wpt,'cfs',35)+read(wpt,'cfs',36)+read(wpt,'cfs',37)+read(wpt,'cfs',38);
-
-meandelta= mean(signaldelta);
-meantheta= mean(signaltheta);
-meanalpha= mean(signalalpha);
-meanbeta= mean(signalbeta);
-
-meanAbsdelta= mean(abs(signaldelta));
-meanAbstheta= mean(abs(signaltheta));
-meanAbsalpha= mean(abs(signalalpha));
-meanAbsbeta= mean(abs(signalbeta));
-
-stdDelta= std(signaldelta);
-stdTheta = std(signaltheta);
-stdAlpha= std(signalalpha);
-stdBeta= std(signalbeta);
-
-energyDelta= sum(signaldelta.^2);
-energyTheta = sum(signaltheta.^2);
-energyAlpha= sum(signalalpha.^2);
-energyBeta= sum(signalbeta.^2);
-
-entropyDelta= approximateEntropy(signaldelta);
-entropyTheta = approximateEntropy(signaltheta);
-entropyAlpha= approximateEntropy(signalalpha);
-entropyBeta= approximateEntropy(signalbeta);
-
-powerDelta= sum(signaldelta.^2)/length(signaldelta);
-powerTheta = sum(signaltheta.^2)/length(signaltheta);
-powerAlpha= sum(signalalpha.^2)/length(signalalpha);
-powerBeta= sum(signalbeta.^2)/length(signalbeta);
-
-skewnessDelta=skewness(signaldelta);
-skewnessTheta=skewness(signaltheta);
-skewnessAlpha=skewness(signalalpha);
-skewnessBeta=skewness(signalbeta);
-
-kurtosisDelta=kurtosis(signaldelta);
-kurtosisTheta=kurtosis(signaltheta);
-kurtosisAlpha=kurtosis(signalalpha);
-kurtosisBeta=kurtosis(signalbeta); 
-
-varDelta=var(signaldelta);
-varTheta=var(signaltheta);
-varAlpha=var(signalalpha);
-varBeta=var(signalbeta); 
+% wpt = wpdec(epoch1,5,'db4','shannon');
+% %plot(wpt)
+% signaldelta=read(wpt,'cfs',31);
+% signaltheta=read(wpt,'cfs',32);
+% signalalpha=read(wpt,'cfs',33);
+% signalbeta=read(wpt,'cfs',34)+read(wpt,'cfs',35)+read(wpt,'cfs',36)+read(wpt,'cfs',37)+read(wpt,'cfs',38);
+% 
+% meandelta= mean(signaldelta);
+% meantheta= mean(signaltheta);
+% meanalpha= mean(signalalpha);
+% meanbeta= mean(signalbeta);
+% 
+% meanAbsdelta= mean(abs(signaldelta));
+% meanAbstheta= mean(abs(signaltheta));
+% meanAbsalpha= mean(abs(signalalpha));
+% meanAbsbeta= mean(abs(signalbeta));
+% 
+% stdDelta= std(signaldelta);
+% stdTheta = std(signaltheta);
+% stdAlpha= std(signalalpha);
+% stdBeta= std(signalbeta);
+% 
+% energyDelta= sum(signaldelta.^2);
+% energyTheta = sum(signaltheta.^2);
+% energyAlpha= sum(signalalpha.^2);
+% energyBeta= sum(signalbeta.^2);
+% 
+% entropyDelta= approximateEntropy(signaldelta);
+% entropyTheta = approximateEntropy(signaltheta);
+% entropyAlpha= approximateEntropy(signalalpha);
+% entropyBeta= approximateEntropy(signalbeta);
+% 
+% powerDelta= sum(signaldelta.^2)/length(signaldelta);
+% powerTheta = sum(signaltheta.^2)/length(signaltheta);
+% powerAlpha= sum(signalalpha.^2)/length(signalalpha);
+% powerBeta= sum(signalbeta.^2)/length(signalbeta);
+% 
+% skewnessDelta=skewness(signaldelta);
+% skewnessTheta=skewness(signaltheta);
+% skewnessAlpha=skewness(signalalpha);
+% skewnessBeta=skewness(signalbeta);
+% 
+% kurtosisDelta=kurtosis(signaldelta);
+% kurtosisTheta=kurtosis(signaltheta);
+% kurtosisAlpha=kurtosis(signalalpha);
+% kurtosisBeta=kurtosis(signalbeta); 
+% 
+% varDelta=var(signaldelta);
+% varTheta=var(signaltheta);
+% varAlpha=var(signalalpha);
+% varBeta=var(signalbeta); 
 %% POWER SPECTRAL DENSITY
 figure(4)
 nfftWelch= 2^nextpow2(L/3); 
@@ -155,7 +177,7 @@ nfftWelch= 2^nextpow2(L/3);
 pwelch(epochs(2,:),hanning(L/3), L/6, nfftWelch,Fs,'psd');
 figure(5)
 power= pwelch(epochs(2,:),hanning(L/3),L/6,nfftWelch, Fs,'power');  %[p,f]= pwelch(x,window,noverlap,nfft,fs) 
-pwelch(epochs(2,:),hanning(L/3),L/6, nfftWelch,Fs,'power'); 
+pwelch(epochs(2,:),hanning(L/3),L/6, nfftWelch,Fs,'power');
 %% AVERAGE BAND POWER FOR EEG EPOCHS
 %compute average band power for each EEG channel using 3 different methods
  
@@ -179,12 +201,18 @@ thetaPerio = bandpower(psdPerio,fPerio,[4,8],'psd');
 alphaPerio= bandpower(psdPerio,fPerio,[8,12],'psd');
 betaPerio= bandpower(psdPerio,fPerio,[12,30],'psd');
 figure(7)
-subplot(2,2,1);
-pwelch(epochs(2,:),hanning(L/3), L/6,nfftWelch,Fs,'psd');
-subplot(2,2,2);
-periodogram(epochs(2,:),hanning(L),nfft,Fs);
-saveas(gcf,'Pwelch_Periodogram_plot','epsc')
-saveas(gcf,'Pwelch_Periodogram_plot', 'jpg')
+
+t = tiledlayout('flow');
+% % Plot in tiles
+nexttile, ax(1)= plot(fWelch,10*log10(psdWelch)), title('Welch Power Spectral Density Estimate','interpreter' ,'latex','fontsize', 18,'FontWeight','bold'),xlim([0 128]) 
+nexttile, ax(2)=  plot(fPerio,10*log10(psdPerio)),title('Periodogram Power Spectral Density Estimate','interpreter', 'latex','fontsize', 18,'fontweight','bold'), xlim([0 128])
+plotcolor(ax,'green', 'colordiff',0);
+% % Specify common title, X and Y labels
+% title(t, 'Decomposition of epoch signal','interpreter', 'latex','fontsize', 18)
+xlabel(t, '$Frequency [Hz]$','interpreter', 'latex', 'fontsize',17)
+ylabel(t, '$Power/frequency [dB/Hz]$','interpreter', 'latex','fontsize',17)
+saveas(gcf,'Pwelch_PerioPsd','epsc')
+
 %% FEATURES
 %TIME DOMAIN
 %mean value
